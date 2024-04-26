4de077c6e76562a6efa3b8d227a991d474bdf0fe
Kseniya <kseniya.d.bakshaeva@openelbrus.ru>
2024-04-26 14:09:11 +0300

    laba 2 zdelana

---
+++ 2/OddEvenSort.asm
@@ -0,0 +1,88 @@
+
+;  swaps values on two addresses in rsi and r11,
+;  sets sorted in rax to false
+
+global Swap
+
+Swap: 
+    xor r9, r9
+    movzx r9, byte [rsi]
+    cmp r9b, byte [r11]
+    jle end_swap
+        xor r9, r9
+        xor r10, r10
+        movzx r9, byte [rsi]
+        movzx r10, byte [r11]
+        mov [rsi], r10b
+        mov [r11], r9b
+        mov rax, 0
+    end_swap: 
+ret 
+
+
+%macro iterate 2
+	add %1, %2
+    inc %1
+%endmacro
+
+;  takes list head, its length and matrix parameter in rdi, r14, r15
+    global OddEvenSort
+
+OddEvenSort:
+    xor rax, rax
+    xor rbx, rbx
+    xor rcx, rcx
+    xor rdx, rdx 
+    xor rsi, rsi
+
+    mov rax, 0           ; sorted
+    mov rdx, r14  ; list length
+    sub rdx, 1
+    mov rbx, rdi ; list head 
+    mov r12, r15 ; matrix parameter
+
+while_loop:
+    mov rax, 1
+
+    mov rcx, 1             ; i
+    mov rsi, rbx           ; diag 0
+    iterate rsi, r12       ; diag 1
+
+for_loop_odd: 
+    cmp rcx, rdx       ; i < list.length - 1
+    jge end_for_loop_odd
+    
+    mov r11, rsi        
+    iterate r11, r12    ; diag next 
+
+    call Swap 
+
+    add rcx, 2          
+    mov rsi, r11        
+    iterate rsi, r12    ; diag cur
+jmp for_loop_odd
+
+end_for_loop_odd:
+
+    mov rcx, 0             ; i
+    mov rsi, rbx           ; list+0
+
+for_loop_even: 
+    cmp rcx, rdx       ; i < list.length - 1
+    jge end_for_loop_even
+    
+    mov r11, rsi       
+    iterate r11, r12   ; diag next 
+
+    call Swap
+
+    add rcx, 2
+    iterate rsi, r12
+    jmp for_loop_even  ; diag cur
+
+end_for_loop_even:
+  
+    cmp rax, 0
+je while_loop
+
+ret
--- 2/my_prog.asm
+++ 2/my_prog.asm
@@ -1,101 +1,22 @@
-% x/16bd &matrix
+; x/16bd &matrix
 
 section .data
 	size:        db 4
 	align 1              
-    matrix          db    1, 6, 3, 9,   \
-						  8, 5, 2, 7,   \
-						 10, 4, 9, 8,  \
+    matrix          db    1, 8, 7, 9,   \
+						  8, 5, 2, 3,   \
+						 10, 4, 9, 6,  \
 						 14, 3, 2, 2   \
+
+;   1  2  3 9
+;   2  5  6 7
+;   3  4  9 8
+;   14 10 8 2
 
 section .text
 	global _start
 
-;  swaps values on two addresses
-%macro swap 2 
-	xor r9, r9
-	xor r10, r10
-	movzx r9, byte [%1]
-	movzx r10, byte [%2]
-    mov [%1], r10
-    mov [%2], r9
-%endmacro
-
-
-%macro iterate 1
-	add %1, [size]
-%endmacro
-
-;  takes list head and its length
-    global OddEvenSort
-
-OddEvenSort:
-    xor rax, rax
-    xor rbx, rbx
-    xor rcx, rcx
-    xor rdx, rdx 
-    xor rsi, rsi
-
-    mov rax, 0           ; sorted
-    mov rdx, qword [rsp + 8]  ; list length
-    sub rdx, 1           ; list.length - 1
-    mov rbx, qword [rsp + 16] ; list head 
-
-while_loop:
-    mov rax, 1
-
-    mov rcx, 1             ; i
-    mov rsi, rbx           ; list+0
-    iterate rsi            ; list+1
-
-for_loop_1: 
-    cmp rcx, rdx       ; i < list.length - 1
-    jge end_for_loop_1
-    
-    mov r11, rsi        ; list+i
-    iterate r11         ; list+i+1
-
-	movzx r9, byte [rsi]
-    cmp r9, [r11]
-    jle end_cond1
-    	swap rsi, r11 
-    	mov rax, 0
-	end_cond1:
-
-    add rcx, 2
-    iterate rsi
-    jmp for_loop_1
-
-end_for_loop_1:
-
-    mov rcx, 0             ; i
-    mov rsi, rbx           ; list+0
-for_loop_2: 
-    cmp rcx, rdx       ; i < list.length - 1
-    jge end_for_loop_2
-    
-    mov r11, rsi        ; list+i
-    iterate r11         ; list+i+1
-
-    movzx r9, byte [rsi]
-    cmp r9, [r11]
-    jle end_cond2
-    	swap rsi, r11
-    	mov rax, 0
-	end_cond2:
-
-    add rcx, 2
-    iterate rsi
-    jmp for_loop_2
-
-end_for_loop_2:
-  
-cmp rax, 0
-je while_loop
-
-ret
-
-
+%include "OddEvenSort.asm"
 
 _start:
 
@@ -103,38 +24,49 @@ movzx r14, byte [size]
 cmp r14, 3
 jl exit 
 
+;  rdi - the second elm of the first line
 mov rdi, matrix
-add rdi, 1
+
+;  r13 - the one before last elm of first line
 mov r13, matrix
 add r13, r14
 sub r13, 1
 
+mov r15, r14    ;  size in r15
+dec r14
+
 upper_part:
-	mov r12, r14
-	sub r12, rdi
-    push rdi
-	push r12
-	call OddEvenSort 
+	; move along the row 
 	add rdi, 1
+
+	call OddEvenSort
+	sub r14, 1
+
 	cmp rdi, r13
 jl upper_part
 
-mov rdi, r13
-mov r13, matrix
 xor rax, rax
-mov rax, r14
-imul r14
+mov rax, r15
+imul r15        ;  rax - the number of elms in mx
+
+mov rdi, matrix 
+
+mov r13, matrix
 add r13, rax
-sub r13, r14
-sub r13, 1
+sub r13, r15 
+sub r13, r15    ; the 1st elm of one before last line 
+
+mov r14, r15
+dec r14
 
 lower_part:
-	mov r12, r14
-	sub r12, rdi
-    push rdi
-	push r12
-	call OddEvenSort 
-	add rdi, r14
+	; move along the column
+	iterate rdi, r15 
+	sub rdi, 1
+
+	call OddEvenSort
+	sub r14, 1
+
 	cmp rdi, r13
 jl lower_part
 
